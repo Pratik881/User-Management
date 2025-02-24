c5a1b2ccf8aa27274d8d86c5b0d4002beb5694ca
Pratik881 <pratikn484@gmail.com>
2025-02-24 23:57:56 +0545

    viewmodel added

---
+++ ViewModels/ForgetPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementSystem.ViewModels
+{
+    public class ForgetPasswordViewModel
+    {
+        [Required]
+        public string email;
+    }
+}
